2dca405e9e89788ecaeaeaff768ba59a94b629ae
fatimamohamed <adropofilm@gmail.com>
2019-01-12 20:07:56 -0600

    tests added

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,35 +1,76 @@
 defmodule Cards do
   @moduledoc """
+    Provides methods for creating and handling deck of cards.
 
   """
+
+  @doc """
+    Returns list of strings representing a deck of playing cards.
+  """
   def create_deck do
-    values = ["A", "2", "3", "4", "5", "6", "7", "8",
-      "9", "10", "J", "Q", "K"]
-    suits = ["S", "C", "H", "D"]
+    values = ["Ace", "2", "3", "4", "5", "6", "7", "8",
+      "9", "10", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamond"]
 
     for suit <- suits, value <- values do
         "#{value} of #{suit}"
     end
   end
 
+  @doc """
+    Returns a deck of shuffled cards.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns boolean for whether or not card in deck.
+
+  ## Example:
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     card in deck
   end
 
+  @doc """
+    Divides deck into hand and reminder of deck.
+    The `hand_size` argument indicates how many
+    cards you want in hand.
+
+  ## Example:
+      iex> deck = Cards.create_deck
+      iex> {hand, _rest_of_deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves deck to computer.
+
+  ## Example:
+      iex> deck = Cards.create_deck
+      iex> Cards.save(deck, "filename")
+      "filename was saved"
+  """
   def save(deck, filename) do
     # convert deck to erlang's def of object & save
     binary = :erlang.term_to_binary(deck)
-    File.write(filename, binary)
+    case File.write(filename, binary) do
+      :ok -> "#{filename} was saved"
+      _ -> "There was an error saving #{filename}"
+    end
   end
 
+  @doc """
+    Opens saved deck.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -37,13 +78,14 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Creates a shuffled deck and divides into a hand of
+    desired size(`hand_size`) and remainder.
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
     |> Cards.deal(hand_size)
-
   end
-
-
 
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,45 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 52 cards" do
+    deck_len = length(Cards.create_deck)
+    assert deck_len == 52
   end
+
+  test "shuffle randomizes deck" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "contains? checks if card in deck" do
+    Cards.create_deck
+    |> Cards.contains?("Ace of Spades")
+    |> assert
+  end
+
+  #TODO figure out better way to do line 25-28
+  test "deal distributes x amount of cards" do
+    Cards.create_deck
+    |> Cards.deal(5)
+    |> case do
+      {hand, rest_of_deck} ->
+        assert length(hand) == 5
+        assert length(rest_of_deck) == 47
+    end
+  end
+
+  test "save deck" do
+    Cards.create_deck
+    |> Cards.save("filename")
+    |> String.equivalent?("filename was saved")
+    |> assert
+  end
+
+  test "load opens saved deck" do
+    comparison_deck = Cards.create_deck
+    loaded_deck = Cards.load("filename")
+    assert loaded_deck == comparison_deck
+  end
+
+
 end
