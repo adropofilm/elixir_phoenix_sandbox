344b5c775c1855e90cf74e76e02b5314391d73c5
fatimamohamed <adropofilm@gmail.com>
2019-01-13 21:27:24 -0600

    Add identicon generating actions

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,16 +9,52 @@ defmodule Identicon do
     |> pick_color
     |> construct_grid
     |> filter_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+
+  @doc """
+    Draw image from color and pixel_map
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
+  end
+
+  @doc """
+    Builds pixel map from grid
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
 
   @doc """
     Filters out odd squares so that only even squares are colored in final image
   """
   def filter_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code, _index}) ->
-      rem(code, 2) === 0
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
     end
+    %Identicon.Image{image | grid: grid}
   end
 
   @doc """
@@ -35,7 +71,7 @@ defmodule Identicon do
       |> List.flatten
       |> Enum.with_index
 
-      %Identicon.Image{ image | grid: grid}
+    %Identicon.Image{image | grid: grid}
   end
 
   @doc """
@@ -49,7 +85,6 @@ defmodule Identicon do
     Use pattern matching to extract RGB values from Identicon.Image struct.
   """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    #create new struct with [r, g, b] from old struct, but with added RGB info
     %Identicon.Image{image | color: {r, g, b}}
   end
 
@@ -63,7 +98,7 @@ defmodule Identicon do
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
     %Identicon.Image{hex: hex}
   end
-
 end
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -3,7 +3,7 @@ defmodule Identicon.Image do
 
 
 """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 
 
 end
